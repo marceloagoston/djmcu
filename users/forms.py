66b6e77639f5ec540549aae2a5f9bd0c1e1b1dc8
Marcelo Agoston <marceloagoston91@gmail.com>
2019-12-03 10:44:15 -0300

    Custom User Model w/o migrations and Custom Form. No DB

---
+++ users/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from .models import CustomUser
+
+#this form it uses to extend the UserCreationform and userchangeform
+class CustomUserCreationForm(UserCreationForm):
+	class Meta(UserCreationForm.Meta):
+		model = CustomUser
+		fields = UserCreationForm.Meta.fields + ('edad',)
+
+class CustomUserChangeForm(UserChangeForm):
+	class Meta:
+		model = CustomUser
+		fields = UserChangeForm.Meta.fields
