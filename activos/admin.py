064d1ae447682cad9594bf68d6e3778c072577e2
Marcelo Agoston <marceloagoston91@gmail.com>
2020-01-17 13:26:35 -0300

    some vistual changes and added filters in Activos management in backend

--- activos/admin.py
+++ activos/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import Activo
 
-admin.site.register(Activo)
+class ActivoAdmin(admin.ModelAdmin):
+	model = Activo
+	list_display = ['TAid','resp_seguridad','date','tipoactivo','nombre','propietario','responsable','ubicacion','valor',]
+	list_filter = ['resp_seguridad','tipoactivo',]
+	search_fields = ['resp_seguridad__username','tipoactivo',]
+
+admin.site.register(Activo, ActivoAdmin)
--- users/admin.py
+++ users/admin.py
@@ -11,3 +11,7 @@ class CustomUserAdmin(UserAdmin):
 	list_display = ['email','username','edad','dependencia','is_staff',]
 
 admin.site.register(CustomUser, CustomUserAdmin)
+# Personalizado del Backend
+admin.site.site_header = 'Seguridad Chaco - Panel de Administración'
+admin.site.site_title = 'Seguridad Chaco'
+admin.site.index_title = 'Panel de Administración'
